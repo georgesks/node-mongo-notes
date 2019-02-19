44fd4e6076554fd8620a65fd5101fd3e2c8c825a
jorge gmes <jorge@pilotoautomatico.mx>
2019-02-19 14:24:54 -0600

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
+
+// Initializations
+const app = express();
+
+// Settings
+app.set("port", process.env.PORT || 3000);
+app.set("view", path.join(__dirname, "views"));
+app.engine(".hbs", exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs"
+}));
+app.set("view engine", ".hbs")
+
+// Middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride("_method"));
+app.use(express.json());
+app.use(session({
+    secret: "mysecretapp",
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Global Variables
+
+// Routes
+app.use(require("./routes/index"));
+app.use(require("./routes/users"));
+app.use(require("./routes/notes"));
+
+//Static Files
+
+// Server on
+app.listen(app.get("port"), () => {
+
+    console.log("Server Ready on Port", app.get("port"));
+
+});
