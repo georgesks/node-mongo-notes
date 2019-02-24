1366764150a7359c72eb730f0bca2f5fbd24e84f
Jorge Gomez <george.gmes@gmail.com>
2019-02-24 15:25:30 -0600

    Avance

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User");
+
 router.get("/users/signin", (req, res) => {
 
     res.render("users/signin");
@@ -10,6 +12,50 @@ router.get("/users/signin", (req, res) => {
 router.get("/users/signup", (req, res) => {
 
     res.render("users/signup");
+
+});
+
+router.post("/users/signup", async (req, res) => {
+
+    const { name, email, password, confirm_password } = req.body;
+    const errors = [];
+
+    if(password != confirm_password){
+        errors.push({text:"Las contraseñas no coinciden."});
+    }
+
+    if(password.length < 4){
+        errors.push({text:"La contraseña debe contener almenos 4 caracteres"});
+    }
+
+    if(errors.length > 0){
+        res.render("users/signup", {
+            errors,
+            name,
+            email,
+            password,
+            confirm_password
+        });
+    }else{
+
+        const emailUser = await User.findOne({email});
+
+        if(emailUser){
+
+            req.flash("error_msg", "El correo ya existe.");
+            res.redirect("/users/signup");
+
+        }else{
+
+            const newUser = new User({name, email, password});
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash("success_msg", "Registro correcto.");
+            res.redirect("/users/signin");
+
+        }
+
+    }
 
 });
 
